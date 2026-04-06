941c848ec5182eeaa2287a508b51b93cda3bac20
Santiago Jorda <santiagojorda@gmail.com>
2026-04-05 22:33:32 -0300

    implementacion de middleware queue con funciones basicas send, start_consuming, stop_consuming y close

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -4,11 +4,44 @@ import string
 from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
-
     def __init__(self, host, queue_name):
-        pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        self.queue_name = queue_name
+        self.channel.queue_declare(queue=self.queue_name)
+
+    def send(self, message):
+        self.channel.basic_publish(
+            exchange='', 
+            routing_key=self.queue_name, 
+            body=message
+        )
+
+    def start_consuming(self, on_message_callback):
+        
+        def internal_callback(ch, method, properties, body):
+            on_message_callback(
+                body, 
+                lambda: ch.basic_ack(delivery_tag=method.delivery_tag),
+                lambda: ch.basic_nack(delivery_tag=method.delivery_tag)
+            )
+
+        self.channel.basic_consume(
+            queue=self.queue_name, 
+            on_message_callback=internal_callback,
+            auto_ack=False
+        )
+        self.channel.start_consuming()
+
+    def stop_consuming(self):
+        if self.channel:
+            self.channel.stop_consuming()
+
+    def close(self):
+        if self.connection and self.connection.is_open:
+            self.connection.close()
+
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
-    
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+       pass
