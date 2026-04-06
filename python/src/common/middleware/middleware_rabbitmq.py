0ec8a8bc72a6f3a9762e60619d2848136fdbf02c
Santiago Jorda <santiagojorda@gmail.com>
2026-04-06 02:07:00 -0300

    implemtnacion de exchange de rabbit con funciones send , close, start_consuming y stop_consuming

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -12,7 +12,7 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def send(self, message):
         self.channel.basic_publish(
-            exchange='', 
+            exchange='',
             routing_key=self.queue_name, 
             body=message
         )
@@ -44,4 +44,50 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def __init__(self, host, exchange_name, routing_keys):
-       pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        self.routing_keys = routing_keys
+        self.exchange_name = exchange_name
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
+    
+    def start_consuming(self, on_message_callback):
+        def internal_callback(ch, method, properties, body):
+            on_message_callback(
+                body, 
+                lambda: ch.basic_ack(delivery_tag=method.delivery_tag),
+                lambda: ch.basic_nack(delivery_tag=method.delivery_tag)
+            )
+
+        result = self.channel.queue_declare(
+            queue='', 
+            exclusive=True
+        )
+        queue_name = result.method.queue
+
+        for routing_key in self.routing_keys:
+            self.channel.queue_bind(
+                exchange=self.exchange_name,
+                queue=queue_name,
+                routing_key=routing_key
+            )
+        self.channel.basic_consume(
+            queue=queue_name,
+            on_message_callback=internal_callback,
+            auto_ack=False
+        )
+        self.channel.start_consuming()
+    
+    def stop_consuming(self):
+        if self.channel:
+            self.channel.stop_consuming()
+    
+    def send(self, message):
+        for routing_key in self.routing_keys:
+            self.channel.basic_publish(
+                exchange=self.exchange_name,
+                routing_key=routing_key,
+                body=message
+            )  
+    def close(self):
+        if self.connection and self.connection.is_open:
+            self.connection.close()
